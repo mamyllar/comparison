99754a4bc1f3e50ce1dcb6a2d34e21c4440cbb52
mamyllar <79257764+mamyllar@users.noreply.github.com>
2023-03-03 11:15:27 +0200

    classes in python

---
+++ inheritance.py
@@ -0,0 +1,34 @@
+class Animal:
+    def __init__(self, numberOfLegs, color, age) -> None:
+        self.numberOfLegs = numberOfLegs
+        self.color = color
+        self.age = age
+
+    def run(self):
+        print("This animal is running on " + self.numberOfLegs + " legs.")
+
+
+class Pet(Animal):
+    def __init__(self, numberOfLegs, color, age, name, owner) -> None:
+        super().__init__(self, numberOfLegs, color, age)
+        self.name = name
+        self.owner = owner
+
+    def printInfo(self):
+        print(self.name + " is owned by " + self.owner)
+
+
+class Dog(Pet):
+    def __init__(self, numberOfLegs, color, age, name, owner, breed) -> None:
+        super().__init__(self, numberOfLegs, color, age, name, owner)
+        self.breed = breed
+
+    def bark(self):
+        print("Woof woof!")
+
+    def run(self):
+        print("This dog is running on " + self.numberOfLegs + "legs.")
+
+
+
+
