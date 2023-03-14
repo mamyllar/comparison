140e0d5d9d8798abf39a4be336586fa8c526bb23
mamyllar <79257764+mamyllar@users.noreply.github.com>
2023-03-14 17:24:56 +0200

    more fixes?

--- ComparisonApp/ComparisonApp/Program.cs
+++ ComparisonApp/ComparisonApp/Program.cs
@@ -9,6 +9,17 @@ namespace ComparisonApp
             Animal test = new Animal(4, "brown", 6);
             test.run();
 
+            Pet pet1 = new Pet(2, "Red", 50, "Charlie", "Maria");
+            Pet pet2 = pet1;
+            pet1.run();
+            pet2.printInfo();
+            pet1.changeOwner("Hammy");
+            pet2.printInfo();
+
+            Dog dog = new Dog(4, "Black and white", 2, "Musti", "Maria", "Dalmatian");
+            dog.run();
+            dog.bark();
+            dog.printInfo();
 
         }
     }
