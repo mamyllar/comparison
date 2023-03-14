1c20737bd43cee8dea8dd555d9fca2bd8fc3f300
Hammy Wong <wonghammy6@gmail.com>
2023-03-14 16:51:51 +0200

    added some objects in class

--- ComparisonApp/ComparisonApp/Program.cs
+++ ComparisonApp/ComparisonApp/Program.cs
@@ -6,8 +6,21 @@ namespace ComparisonApp
     {
         static void Main(string[] args)
         {
-            Animal test = new Animal(4, "black", 4);
+            Animal test = new Animal(4, "brown", 6);
             test.run();
+
+            Pet pet1 = new Pet(2, "Red", 50, "Charlie", "Maria");
+            Pet pet2 = Pet pet1
+            pet1.run();
+            pet2.printInfo();
+            pet1.changeOwner("Hammy");
+            pet2.printInfo();
+
+            Dog dog = new Dog(4, "Black and white", 2, "Musti", "Maria", "Dalmatian")
+            dog.run();
+            dog.bark();
+            dog.printInfo();
+
         }
     }
 }
