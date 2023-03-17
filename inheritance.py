cc4251b7689d1b93d8ec00942218247a2c63d036
mamyllar <79257764+mamyllar@users.noreply.github.com>
2023-03-17 10:30:15 +0200

    Removed some printouts from python

--- inheritance.py
+++ inheritance.py
@@ -9,7 +9,7 @@ class Animal:
 
 
 class Pet(Animal):
-    def __init__(self, numberOfLegs, color, age, name, owner) -> None:
+    def __init__(self, numberOfLegs, color, age, name, owner):
         super().__init__(numberOfLegs, color, age)
         self.name = name
         self.owner = owner
@@ -23,7 +23,7 @@ class Pet(Animal):
 
 
 class Dog(Pet):
-    def __init__(self, numberOfLegs, color, age, name, owner, breed) -> None:
+    def __init__(self, numberOfLegs, color, age, name, owner, breed):
         super().__init__(numberOfLegs, color, age, name, owner)
         self.breed = breed
 
@@ -40,13 +40,8 @@ pet1 = Pet(2, "Red", 50, "Charlie", "Maria")
 pet2 = pet1
 pet1.run()
 pet2.printInfo()
-print(pet1 == pet2)
 pet1.changeOwner("Hammy")
 pet2.printInfo()
-print(pet1 == pet2)
-pet3 = Pet(2, "Red", 50, "Charlie", "Hammy")
-print(pet1 == pet3)
-print(pet1.__eq__(pet3))
 
 dog = Dog(4, "Black and white", 2, "Musti", "Maria", "Dalmatian")
 dog.run()
